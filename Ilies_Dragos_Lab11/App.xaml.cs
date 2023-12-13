6ec2549e8f65b9b6926a1843ca20f5ceb8a11838
Dragos <ilies.dragos2002@yahoo.com>
2023-12-13 10:56:52 +0200

    Add project files.

---
+++ Ilies_Dragos_Lab11/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Ilies_Dragos_Lab11.Data;
+
+namespace Ilies_Dragos_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Ilies_Dragos_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ilies_Dragos_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
